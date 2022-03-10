c00af1cf550f25641d9a90c1c71671296daf19ed
Stephanie Powell <spowell519@gmail.com>
2022-03-10 15:56:20 -0500

    Sequelize Essentials

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-const { db } = require('./models');
+const { db, Page, User } = require('./models');
 
 db.authenticate()
     .then(() => {
         console.log('connected to the database');
     })
+const initSync = async () => {
+    await db.sync({ force: true });
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`);
+    });
+}
 
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+initSync();
 
